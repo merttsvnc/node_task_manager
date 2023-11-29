6a3d2f96ec03c1347d2b9bef048e196e803a7e72
merttsvnc <merttsvnc@gmail.com>
2023-11-29 20:31:58 +0900

    Add start and dev scripts to package.json, and add express dependency

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/task-manager-api', {
@@ -7,69 +6,3 @@ mongoose
   })
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB...', err))
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) throw new Error('Email is invalid')
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password'))
-        throw new Error('Password cannot contain "password"')
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) throw new Error('Age must be a positive number')
-    },
-  },
-})
-
-const me = new User({
-  name: '   Serhat mertt',
-  email: 'mert@mert.com       ',
-  password: 'phone098!',
-})
-
-me.save()
-  .then(() => console.log(me))
-  .catch((err) => console.error('Error:', err))
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-})
-
-// const task = new Task({
-//   description: 'Learn the Mongoose library',
-//   completed: false,
-// })
-
-// task
-//   .save()
-//   .then(() => console.log(task))
-//   .catch((err) => console.error('Error:', err))
---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body)
+
+  user
+    .save()
+    .then(() => res.status(201).send(user))
+    .catch((err) => res.status(400).send(err))
+})
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => res.status(500).send(err))
+})
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params
+
+  User.findById(id)
+    .then((user) => {
+      if (!user) return res.status(404).send()
+      res.send(user)
+    })
+    .catch((err) => res.status(500).send(err))
+})
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body)
+
+  task
+    .save()
+    .then(() => res.status(201).send(task))
+    .catch((err) => res.status(400).send(err))
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
