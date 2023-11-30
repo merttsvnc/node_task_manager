38e8cdfab293353cef369791a08872b82eb145c7
merttsvnc <merttsvnc@gmail.com>
2023-11-30 17:35:23 +0900

    Fix JWT_KEY to JWT_SECRET in user.js and add authentication middleware

--- src/models/user.js
+++ src/models/user.js
@@ -49,7 +49,7 @@ const userSchema = new mongoose.Schema({
 // custom method to generate authToken
 userSchema.methods.generateAuthToken = async function () {
   const user = this
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_KEY)
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
   user.tokens = user.tokens.concat({ token })
   await user.save()
   return token
