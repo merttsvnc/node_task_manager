6bdd91633260ab802bc28f4d9dc1c8affa72538f
merttsvnc <merttsvnc@gmail.com>
2023-11-30 14:56:52 +0900

    Add token generation for user authentication

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,7 +6,8 @@ router.post('/users', async (req, res) => {
   try {
     const user = new User(req.body)
     await user.save()
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
   } catch (error) {
     res.status(400).send(error)
   }
