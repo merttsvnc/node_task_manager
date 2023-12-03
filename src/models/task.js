6af3ceb36375653759f4895359667a18df26920a
merttsvnc <merttsvnc@gmail.com>
2023-12-03 20:56:47 +0900

    Refactor task and user models. Added timestamp

--- src/models/task.js
+++ src/models/task.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose')
 
-const taskSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-    trim: true,
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: String,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      // This is the reference to the User model
+      ref: 'User',
+    },
   },
-  completed: {
-    type: String,
-    default: false,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    // This is the reference to the User model
-    ref: 'User',
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 const Task = mongoose.model('Task', taskSchema)
 
