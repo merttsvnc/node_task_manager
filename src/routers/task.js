b8e026b9abbd719ba55f9bb5175c121d254692c7
merttsvnc <merttsvnc@gmail.com>
2023-12-03 21:19:12 +0900

    Refactor task router to include query parameters

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,9 +20,28 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const part = req.query.sortBy.split(':')
+    sort[part[0]] = part[1] === 'desc' ? -1 : 1
+  }
+
   try {
-    // const tasks = await Task.find({ owner: req.user._id })
-    await req.user.populate('tasks')
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    })
     res.send(req.user.tasks)
   } catch (err) {
     res.status(500).send(err)
