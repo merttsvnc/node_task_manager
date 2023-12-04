314eba0594b208389cd2910a4b1d7e48c4235931
merttsvnc <merttsvnc@gmail.com>
2023-12-04 12:25:06 +0900

    Add image resizing functionality using Sharp library

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 
 router.post('/users', async (req, res) => {
   try {
@@ -96,7 +97,12 @@ router.post(
   auth,
   upload.single('avatar'),
   async (req, res) => {
-    req.user.avatar = req.file.buffer
+    // req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
   },
@@ -116,7 +122,7 @@ router.get('/users/:id/avatar', async (req, res) => {
     const user = await User.findById(req.params.id)
     if (!user || !user.avatar)
       throw new Error('Unable to find user or user avatar')
-    res.set('Content-Type', 'image/jpg')
+    res.set('Content-Type', 'image/png')
     res.send(user.avatar)
   } catch (err) {
     res.status(404).send()
